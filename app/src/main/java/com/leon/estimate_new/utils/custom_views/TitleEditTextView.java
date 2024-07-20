6171faf7354332e8495f6bb21088e304eaadbcc3
Leon <ali.rostami33@gmail.com>
2024-07-20 17:36:14 +0330

    init first custom view

---
+++ app/src/main/java/com/leon/estimate_new/utils/custom_views/TitleEditTextView.java
@@ -0,0 +1,25 @@
+package com.leon.estimate_new.utils.custom_views;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class TitleEditTextView extends View {
+    public TitleEditTextView(Context context) {
+        super(context);
+    }
+
+    public TitleEditTextView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public TitleEditTextView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public TitleEditTextView(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+}
