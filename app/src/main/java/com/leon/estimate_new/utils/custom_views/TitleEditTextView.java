4912af00ced133a4771c733202bf954d10d94d2e
Leon <ali.rostami33@gmail.com>
2024-07-21 10:42:08 +0330

    redesign has been started

--- app/src/main/java/com/leon/estimate_new/utils/custom_views/TitleEditTextView.java
+++ app/src/main/java/com/leon/estimate_new/utils/custom_views/TitleEditTextView.java
@@ -2,9 +2,12 @@ package com.leon.estimate_new.utils.custom_views;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.LayoutInflater;
 import android.view.View;
 
 import androidx.annotation.Nullable;
+
+import com.leon.estimate_new.R;
 
 public class TitleEditTextView extends View {
     public TitleEditTextView(Context context) {
@@ -13,6 +16,7 @@ public class TitleEditTextView extends View {
 
     public TitleEditTextView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        inflate(context,R.layout.title_edit_text,this);
     }
 
     public TitleEditTextView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
